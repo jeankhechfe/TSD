6205c0ae6bb0e4fc19eda2d75019fd6e10f40dcd
Jean Khechfe <jeankhechfe@gmail.com>
2020-03-25 20:31:01 +0100

    04-Yummy second task

--- 04-Yummy/Models/Recipe.cs
+++ 04-Yummy/Models/Recipe.cs
@@ -6,12 +6,18 @@ namespace Yummy.Models
     public class Recipe
     {
         public int Id { get; set; }
+
+        [MaxLength(30)]
         public string Name { get; set; }
 
         [DataType(DataType.Date)]
         public DateTime Time { get; set; }
+
+        [Range(1, 5)]
         public int Difficulty { get; set; }
         public int Number_of_likes { get; set; }
+
+        [Required]
         public string Ingredients { get; set; }
         public string Process { get; set; }
         public string Tips_and_tricks{ get; set; }
